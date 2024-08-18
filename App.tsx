80b31b6c5d8c9c1e3dcbfb455d72b132c6a54b60
Eric Laursen <ericjlaursen@gmail.com>
2024-08-18 16:18:52 -0400

    trial-by-style

--- App.tsx
+++ App.tsx
@@ -1,20 +1,46 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { PixelRatio, StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+/*
+  React-Native v. html elements
+  View
+    div
+  Text
+    p, etc.
+
+
+  
+  - CONDITIONS can be a little different in react-native
+    - falsey will render on the screen
+    - try for {NaN ? ...val : null}
+  
+*/
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
   },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          borderBottomWidth: 1,
+          borderBottomColor: "#1a759f",
+          paddingHorizontal: 8,
+          paddingVertical: 16,
+        }}
+      >
+        <Text style={{ fontSize: 18, fontWeight: "200" }}>
+          Open up App.tsx to start working on your app!
+        </Text>
+        <Text>{PixelRatio.get()} Pixel Ratio</Text>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
