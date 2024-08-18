d7a2daae7bc25248ebccc3dbf3e12d5d5a04bf1b
Eric Laursen <ericjlaursen@gmail.com>
2024-08-18 16:36:55 -0400

    add a "delete" button to the item

--- App.tsx
+++ App.tsx
@@ -1,6 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { PixelRatio, StyleSheet, Text, View } from "react-native";
-
+import {
+  TouchableOpacity,
+  // PixelRatio,
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+} from "react-native";
+import { theme } from "./theme";
 /*
   React-Native v. html elements
   View
@@ -13,16 +20,31 @@ import { PixelRatio, StyleSheet, Text, View } from "react-native";
   - CONDITIONS can be a little different in react-native
     - falsey will render on the screen
     - try for {NaN ? ...val : null}
-  
+  - React-Native Button is never really used - it can't be styled
+
 */
 
 const styles = StyleSheet.create({
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: {
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
+    color: theme.colorWhite,
+  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
     justifyContent: "center",
   },
   itemContainer: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
     borderBottomWidth: 1,
     borderBottomColor: "#1a759f",
     paddingHorizontal: 8,
@@ -32,14 +54,37 @@ const styles = StyleSheet.create({
 });
 
 export default function App() {
-  const { container, itemContainer, itemText } = styles;
+  const { button, buttonText, container, itemContainer, itemText } = styles;
+
+  const handleDelete = () => {
+    Alert.alert(
+      "Are you sure you want to delete this?",
+      "It will be gone for good",
+      [
+        {
+          text: "Yes",
+          onPress: () => console.log("deleting..."),
+          style: "destructive",
+        },
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+      ],
+    );
+  };
+
   return (
     <View style={container}>
       <View style={itemContainer}>
-        <Text style={itemText}>
-          Open up App.tsx to start working on your app!
-        </Text>
-        <Text>{PixelRatio.get()} Pixel Ratio</Text>
+        <Text style={itemText}>Coffee</Text>
+        <TouchableOpacity
+          onPress={handleDelete}
+          activeOpacity={0.8}
+          style={button}
+        >
+          <Text style={buttonText}>Delete</Text>
+        </TouchableOpacity>
       </View>
       <StatusBar style="auto" />
     </View>
