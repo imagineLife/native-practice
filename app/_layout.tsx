aa974ceec2c26262ab34aec49dd398309267c797
Eric Laursen <ericjlaursen@gmail.com>
2024-08-18 21:36:29 -0400

    some stack options, playing with navs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,11 +3,24 @@ import { Stack } from "expo-router";
 /*
   screens are in the stack
   - a new page/screen will render "ontop" of the previous screen, in a "stack"!
+  - the ONLY reason routes need defining here is to pass props to the pages
 */
 export default function AppLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Shopping List" }} />
+      <Stack.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          presentation: "modal",
+          animation: "slide_from_bottom",
+        }}
+      />
+      <Stack.Screen
+        name="idea"
+        options={{ title: "Idea", presentation: "modal" }}
+      />
     </Stack>
   );
 }
